65cdedbbe7247f51dedd2a85e2c04e3dcf3ddc14
Wellington Santiago <Wellingtonhmt2011@gmail.com>
2022-01-15 18:24:12 -0300

    adicionando footer com meus links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Checkbox from './Form/Checkbox';
 import Quest from './questComponents/Quest';
 import Position from './questComponents/Position';
 import Options from './questComponents/Options';
-
+import FooterCopy from './FooterCopy';
 import Score from './questComponents/Score';
 const App = () => {
   const cores = ['tomato', 'aqua', 'lime'];
@@ -121,28 +121,19 @@ const App = () => {
             />
             <Position perguntas={perguntas} perguntaAtual={perguntaAtual} />
           </form>
-          <footer style={{ textAlign: 'center', color: '#ccc' }}>
-            <p> Criado por Wellington Santiago </p>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/wellington-santiago-161201201/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BU8bIlIDgRoKcUDXPThs1dQ%3D%3D"
-            >
-              linkedim
-            </a>
-            <br />
-            <a target="_blank" href="https://github.com/dshzr/">
-              github
-            </a>
-          </footer>
+          <FooterCopy />
         </>
       ) : (
-        <Score
-          alignCenterStyle={alignCenterStyle}
-          acertos={acertos}
-          perguntas={perguntas}
-          reiniciar={reiniciar}
-          acertosStyle={acertosStyle}
-        />
+        <>
+          <Score
+            alignCenterStyle={alignCenterStyle}
+            acertos={acertos}
+            perguntas={perguntas}
+            reiniciar={reiniciar}
+            acertosStyle={acertosStyle}
+          />
+          <FooterCopy />
+        </>
       )}
     </>
   );
